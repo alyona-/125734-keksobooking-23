d49b77a406f037a0cf379b8be8b496652015e778
Alyona <alyonajs@yandex.ru>
2021-06-08 20:22:29 +0300

    Редактирование 2-ого задания

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 // eslint-disable-next-line no-unused-vars
-const getRandomNum =(min, max, decimalPlaces=0) =>{
-  if(decimalPlaces<0 || decimalPlaces>20)  {decimalPlaces=0;}
-  return (Math.random( ) * (max - min + 1) + min).toFixed(decimalPlaces);
+const getRandomNum = (min, max) => {
+  if (min < 0 || max <= min) {
+    return null;
+  }
+  return (Math.random() * (max - min + 1) + min);
 };
 
+// eslint-disable-next-line no-unused-vars
+const getDecimalPlaces = (num, decimalPlaces) => {
+  if (decimalPlaces < 0 || decimalPlaces > 20 || num === null) {
+    return null;
+  }
+  return num.toFixed(decimalPlaces);
+};
+
+
 // eslint-disable-next-line no-console
-console.log(getRandomNum(2,5,20));
+console.log(getDecimalPlaces(getRandomNum(6, 12), 3));
